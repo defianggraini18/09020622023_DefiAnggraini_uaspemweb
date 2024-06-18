aff1bb3e57dd3156461ce8e2a396223dd6b9f1f3
Defi Anggraini <defdefiaa@gmail.com>
2024-06-18 09:18:41 +0700

    Create server.js

---
+++ backend/server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');  
+
+const app = express();
+const port = 5000;
+
+const db = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'javawebmedia_webci4',
+    port: 3307
+});
+
+db.connect(err => {
+    if (err) {
+        console.error('Database connection error:', err.stack);
+        return;
+    }
+    console.log('Connected to database.');
+});
+
+
+app.use(cors());  
+
+
+app.get('/', (req, res) => {
+    db.query('SELECT * FROM berita', (err, results) => {
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(results);
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
